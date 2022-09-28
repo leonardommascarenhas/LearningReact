604cd1adcee024a6d113772dfe14367b693f7fdd
leonardo <leonardommascarenhas@gmail.com>
2022-09-28 16:29:31 -0300

    First

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,6 +2,18 @@ import React from "react";
 
 export class Header extends React.Component {
   render() {
-    return <nav className="navbar navbar-default"></nav>;
+    return (
+      <nav className="navbar navbar-default">
+        <div className="container">
+          <div className="navbar-header">
+            <ul className="nav navbar-nav">
+              <li>
+                <a href="#">Home</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    );
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,11 +1,23 @@
 import React from "react";
 import { render } from "react-dom";
 
+import { Header } from "./components/Header";
+import { Home } from "./components/Home";
+
 class App extends React.Component {
   render() {
     return (
-      <div>
-        <h1>Hello!</h1>
+      <div className="container">
+        <div className="row">
+          <div className="col-xs-10 col-xs-offset-1">
+            <Header />
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-xs-10 col-xs-offset-1">
+            <Home />
+          </div>
+        </div>
       </div>
     );
   }
